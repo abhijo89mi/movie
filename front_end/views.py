7ae55bdc0269d118f9cc0dc4e22ed247eea9b144
Abhilash joseph C <abhi.jo89@gmail.com>
2012-02-04 09:36:33 +0530

    Registration from Bgu fix

--- front_end/views.py
+++ front_end/views.py
@@ -11,6 +11,7 @@ from movie.main.form import *
 from django.contrib.auth import authenticate, login, logout
 from movie.helper import *
 from models import *
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User, Group
 from django.core.mail import EmailMessage
 
@@ -34,6 +35,7 @@ def register_view(request,template='popup/register.html'):
 	context={'form':form }
 	return render_to_response(template, context, context_instance = RequestContext(request)) 
 	
+@csrf_exempt
 def register(request,to_return=''):
 	if request.method == 'POST':
 			form = RegistrationForm(data=request.POST, files=request.FILES)
@@ -44,12 +46,12 @@ def register(request,to_return=''):
 					profile = UserProfile.objects.create(user=new_user) 
 					profile.send_me_daily_email=False
 					profile.save()
-					sendusermail(form.cleaned_data['email'])
+					#sendusermail(form.cleaned_data['email'])
 					login(request, user)
 					to_return={'success':True ,'url':reverse('edit_profile_page')}
 					#return HttpResponseRedirect(reverse('edit_profile_page'))
 			else:
-				to_return={'success':False,'message':form.errors,}
+				to_return={'success':False,'message':''}
 	return HttpResponse(simplejson.dumps(to_return), mimetype='application/json')
 	
 def myaccount(request):
