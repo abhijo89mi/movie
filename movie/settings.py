e277690820209023b9cf2af8980713007bb3900f
Abhilash joseph C <abhi.jo89@gmail.com>
2012-02-18 15:19:34 +0530

    Template design

--- movie/settings.py
+++ movie/settings.py
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.request",
+    'django.core.context_processors.request',
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
--- movie/urls.py
+++ movie/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('movie.main',
     url(r'^$', 'views.index', name='index'),
     url(r'^home/$', 'views.home', name='home'),
     url(r'^logout/$', 'views.logout_view', name='logout'),
+    url(r'^movie/(?P<movie_id>.*)/$','views.movie_info',name='movie'),
     
     
     # ============ URL FOR USER TRACKING=====================
@@ -28,6 +29,9 @@ urlpatterns += patterns('front_end',
       #From submit
       url(r'^register$','views.register',name='register'),
       url(r'^home/myaccount/$','views.myaccount',name='myaccount'),
+      url(r'^home/profile/(?P<user_id>.*)/$', 'views.myaccount', name='user_page'),
+      url(r'user/request/(?P<user_id>.*)/$', 'views.friend_request', name='friend_request'),
+      url(r'user/accept/(?P<user_id>.*)/$', 'views.friend_accept', name='friend_accept'),
       url(r'^home/myaccount/edit_profile_page/$','views.edit_profile_page',name='edit_profile_page'),
       url(r'home/myaccount/edit_profile_page/delete-pic/$', 'views.delete_pic', name='delete_pic'),
       url(r'home/myaccount/edit_profile_page/profile-pic/$', 'views.profile_pic', name='profile_pic'),
